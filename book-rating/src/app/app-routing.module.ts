7af33ee50d65af103a9251fba6e339a557fc4cdc
Ferdinand Malcher <ferdinand@malcher.media>
2019-09-18 15:40:13 +0200

    Routing

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // "pathMatch: full", damit Pfad nicht als Präfix der anderen Ruten interpretiert wird
+  // nötig hier bei leerem Pfad mit Resdirect
+  { path: '', redirectTo: 'books', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- book-rating/src/app/books/books.module.ts
+++ book-rating/src/app/books/books.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BookComponent } from './book/book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { BooksRoutingModule } from './books-routing.module';
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    BookComponent
+    BookComponent,
+    BookDetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    BooksRoutingModule
   ],
   exports: [
     DashboardComponent
