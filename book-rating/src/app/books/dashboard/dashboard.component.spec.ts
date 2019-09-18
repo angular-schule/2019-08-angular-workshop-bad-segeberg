bf03f6dd58225e0a605bb85a0c125b237d0e42ab
Ferdinand Malcher <ferdinand@malcher.media>
2019-09-18 14:03:52 +0200

    Tests Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
+import { BookComponent } from '../book/book.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
+import { when } from 'q';
 
-describe('DashboardComponent', () => {
+fdescribe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  let ratingMock;
+  let book: Book;
+
+  beforeEach(() => {
+    book = {
+      isbn: '',
+      title: '',
+      description: '',
+      rating: 3
+    };
+
+    ratingMock = {
+      rateUp: () => book
+    };
+  });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // Abhängigkeit ersetzen: statt echtem Service immer ratingMock verwenden
+        { provide: BookRatingService, useValue: ratingMock }
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +46,17 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call service for doRateUp()', () => {
+    const rs = TestBed.get(BookRatingService);
+    // callThrough leitet originalen Methodenaufruf durch,
+    // damit Service tatsächlich ein Buch liefert und
+    // updateList() im Dashboard korrekt arbeiten kann
+    spyOn(rs, 'rateUp').and.callThrough();
+
+    component.doRateUp(book);
+
+    expect(rs.rateUp).toHaveBeenCalled();
+  });
+
 });
