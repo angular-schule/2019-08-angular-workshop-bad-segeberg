f01511dac02bcb65d8fc6eb24886b9b95dd3f77a
Ferdinand Malcher <ferdinand@malcher.media>
2019-09-20 09:36:29 +0200

    Fun with Observables

--- book-rating/src/app/app.component.ts
+++ book-rating/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './shared/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-root',
@@ -9,7 +10,35 @@ import { AuthService } from './shared/auth.service';
 export class AppComponent {
   title = 'Book Rating';
 
-  constructor(private as: AuthService) {}
+  constructor(private as: AuthService) {
+
+
+    const producer = function(o) {
+      o.next(1);
+      o.next(2);
+      setTimeout(() => o.next(3), 2000);
+      setTimeout(() => o.complete(), 3000);
+    }
+
+    const observer = {
+      next: e => console.log(e),
+      error: err => console.error(err),
+      complete: () => console.log('Wochenende!')
+    };
+
+    // producer(observer);
+
+
+
+    const myObs$ = new Observable(producer);
+    myObs$.subscribe(observer);
+
+
+
+
+
+
+  }
 
   login() {
     this.as.authorize();
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BookStoreService } from '../shared/book-store.service';
+import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'br-dashboard',
