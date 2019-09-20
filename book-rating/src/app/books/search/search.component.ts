5d3a5769ae5565c58c09a4cefb9385d90e05c97d
Ferdinand Malcher <ferdinand@malcher.media>
2019-09-20 14:11:43 +0200

    Typeahead Search

--- book-rating/src/app/books/search/search.component.ts
+++ book-rating/src/app/books/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { BookStoreService } from '../shared/book-store.service';
+import { filter, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Book } from '../shared/book';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'br-search',
@@ -9,16 +13,29 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class SearchComponent implements OnInit {
 
   searchForm: FormGroup;
+  results$: Observable<Book[]>;
 
-  constructor() { }
+  constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
     this.searchForm = new FormGroup({
       search: new FormControl('')
     });
 
-    this.searchForm.get('search').valueChanges
-      .subscribe(e => console.log(e));
+    // Suchbegriff minimal 3 Zeichen
+    // nicht zu viele Requests, Datenstrom bremsen (Zeit 1 Sek.)
+    // niemals zwei gleiche Suchen hintereinander
+    // HTTP-Request this.bs.search() ✅
+    // Ergebnisse darstellen (ganz einfach!)
+    // AsyncPipe
+
+
+    this.results$ = this.searchForm.get('search').valueChanges.pipe(
+      filter(term => term.length >= 3),
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap(term => this.bs.search(term))
+    );
   }
 
 }
